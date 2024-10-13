94b6fe1dd1ad4f454eaa60b891f1b2cfeb199af0
AntoCreed777 <abenavides@desem.cl>
2024-10-13 15:27:24 -0300

    refactor: Modularizacion en main.c

--- src/main.c
+++ src/main.c
@@ -6,30 +6,34 @@
 #include "memoria.h"
 #include "dfs.h"
 
-int main(){
-    int **grafo = NULL; // Lista de adyacencia indexado a 0
-    int n_vertices;
-    entrada_grafo(&grafo, &n_vertices); // Se obtiene la lista de adyacencia (hay que liberar memoria)
-
-    // Se imprime la lista de adyacencia
-    /*
+void impresion_grafo(int **grafo, int n_vertices){
     for(int i = 0; i < n_vertices; i++){
         for(int j = 0; grafo[i][j] != -1; j++){
             printf(AMARILLO "%d " RESET_COLOR,grafo[i][j]);
         }
         printf("\n");
     }
-    */
+}
 
-    // Lista con los vertices que se ignoraran
-    int ignorados[] = {0, 4, 2, -1}; // Se indica el fin de la lista con -1
-
-    // Se verifica si el grafo es conexo
-    int conexo = dfs_coneccidad(grafo, n_vertices, ignorados);
+void impresion_resultado_dfs(int conexo){
     if(conexo == true) printf(VERDE "El grafo es conexo\n" RESET_COLOR);
     else if(conexo == false) printf(MAGENTA "El grafo no es conexo\n" RESET_COLOR);
     else printf(ROJO "Error al verificar la conexidad del grafo\n" RESET_COLOR);
+}
 
+int main(){
+    int **grafo = NULL; // Lista de adyacencia indexado a 0
+    int n_vertices;
+    entrada_grafo(&grafo, &n_vertices); // Se obtiene la lista de adyacencia (hay que liberar memoria)
+
+    // Se imprime la lista de adyacencia
+    //impresion_grafo(grafo, n_vertices);
+
+    // Lista con los vertices que se ignoraran
+    int ignorados[] = {0, -1}; // Se indica el fin de la lista con -1
+
+    // Se verifica si el grafo es conexo
+    impresion_resultado_dfs(dfs_coneccidad(grafo, n_vertices, ignorados));
 
     liberacion_memoria_grafo(grafo, n_vertices); // Se libera la memoria de la lista de adyacencia
     return 0;
